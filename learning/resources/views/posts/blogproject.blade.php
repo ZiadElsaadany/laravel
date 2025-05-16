3dd74c5cd27a3f596c2ca8c7897fe7a16a151a94
ziad <zz>
2025-05-16 22:08:43 +0300

    template inheritance for shared components

--- learning/resources/views/posts/blogproject.blade.php
+++ learning/resources/views/posts/blogproject.blade.php
@@ -1,34 +1,9 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
-  </head>
-  <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Home</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">AllPosts For First Laravel Project</a>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
-
-
-   <div class="text-center mt-4">
+@extends("layouts.app")
+@section("allposts")
+@section('title')
+posts
+ @endsection
+ <div class="text-center mt-4">
     <br>
     <div >
 <button type="button" class="btn btn-success">Create Post</button>
@@ -63,8 +38,8 @@
 
         {{-- shortcut name and parameter to routing  --}}
         <a href="{{route('posts.show',parameters: $post['id'])}}"   conte  type="button" class="btn btn-primary">View</button>
-<a type="button" class="btn btn-danger">Danger</button>
-<a type="button" class="btn btn-info">Info</button>
+<a type="button" class="btn btn-danger">Delete</button>
+<a type="button" class="btn btn-info">Edit</button>
 
       </td>
     </tr>
@@ -77,21 +52,4 @@
 
    </div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
-
-
-
-</body>
-</html>
+@endsection
